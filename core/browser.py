62ce73c8759222066cf2adaf1ad2cdaa6d475979
gabriellso <gabooliveira2522@gmail.com>
2026-05-07 13:08:11 -0300

    refactor: melhora estrutura do projeto

--- core/browser.py
+++ core/browser.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def start_browser():
 
     options = webdriver.ChromeOptions()
+
     options.add_argument("--start-maximized")
 
     driver = webdriver.Chrome(
-        service=Service(),
+        service=Service(ChromeDriverManager().install()),
         options=options
     )
 
--- core/excel.py
+++ core/excel.py
@@ -5,4 +5,7 @@ def load_excel(path):
 
     df = pd.read_excel(path)
 
+    # remove espaços invisíveis
+    df.columns = df.columns.str.strip()
+
     return df.to_dict(orient="records")
